a54b7297acea2049527842c2dbaf64182c43fc3a
https://learn.co/essential-tutorial-5631 <dkwehrlin.99@gmail.com>
2020-07-09 21:43:51 -0400

    Added method

--- lib/services/dad_jokes_cli.rb
+++ lib/services/dad_jokes_cli.rb
@@ -19,8 +19,8 @@ class DadJokesCLI
     end 
 
     def welcome 
-        puts "Welcome to Everday Dad Jokes!\n"
-        puts "\nAll the dad jokes you could ever need!\n"
+        puts "\nWelcome to Everday Dad Jokes!\n"
+        puts "\nAll the dad jokes you could ever need!\n\n"
     end 
 
     def menu 
@@ -56,21 +56,25 @@ class DadJokesCLI
 
     def print_all_dad_jokes
         DadJokes.all.each do |dad_joke|
-            puts "\n#{dad_joke.punchline} \n #{dad_joke.setup}\n\n"
+            puts "\nSetup: #{dad_joke.punchline} \nPunchline: #{dad_joke.setup}\n\n"
         end 
         nil 
     end 
 
     def print_dad_joke_of_the_day
         dad_joke_of_the_day = DadJokes.all.sample
-        puts "\n#{dad_joke_of_the_day.punchline} \n #{dad_joke_of_the_day.setup}\n\n"
+        puts "\nSetup: #{dad_joke_of_the_day.punchline} \nPunchline: #{dad_joke_of_the_day.setup}\n\n"
     end
     
     def print_number_of_dad_jokes
         puts "\nHow many dad jokes would you like to see? \n"
         puts "Please enter a number anywhere from 1 - 50 below:"
         self.input = gets.chomp
-        num_of_dad_jokes = DadJokes.all.to_a.sample(self.input.to_i)
-        puts "\n#{num_of_dad_jokes.punchline}\n\n#{num_of_dad_jokes.setup}\n\n"
+        num_of_dad_jokes = DadJokes.all.to_a 
+        num_of_dad_jokes.sample(self.input.to_i).each do |dad_joke|
+            puts "\nSetup: #{dad_joke.punchline}\n\nPunchline: #{dad_joke.setup}\n\n"
+        end 
+            
+        # num_of_dad_jokes = DadJokes.all.to_a.sample(self.input.to_i)
     end 
 end 
